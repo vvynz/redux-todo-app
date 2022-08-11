f1321ed42ce35af9539bed9b24c63dac34438ed9
Vivian Zhang <vvynz12@gmail.com>
2022-08-11 18:29:33 -0400

    Create a todoSlice variable and pass it the createSlice function. In the function, pass it a name and initialState property

--- starter/src/redux/todoSlice.js
+++ starter/src/redux/todoSlice.js
@@ -1,1 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const todoSlice = createSlice({
+  name: "todos",
+  initialState: [
+    { id: 1, title: "todo1", completed: false },
+    { id: 2, title: "todo2", completed: false },
+    { id: 3, title: "todo3", completed: true },
+  ],
+});
