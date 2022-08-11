8fca945af28c41050a87b24db9b6b7fc92129a01
Vivian Zhang <vvynz12@gmail.com>
2022-08-11 19:47:43 -0400

    Create and implement a toggleComplete reducer

--- starter/src/redux/todoSlice.js
+++ starter/src/redux/todoSlice.js
@@ -18,8 +18,14 @@ const todoSlice = createSlice({
       };
       state.push(newTodo);
     },
+    toggleComplete: (state, action) => {
+      // find the index of the todo in the array, that we want to update
+      const index = state.findIndex((todo) => todo.id === action.payload.id);
+
+      state[index].completed = action.payload.completed;
+    },
   },
 });
 
-export const {addTodo} = todoSlice.actions;
+export const { addTodo } = todoSlice.actions;
 export default todoSlice.reducer;
